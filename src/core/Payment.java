b7cd6a793a814a8c05d78dd5d165272f79535a46
mohsen-dev90 <mohsen.alho2015@gmail.com>
2020-08-06 14:23:49 +0430

    Payment

--- src/core/Payment.java
+++ src/core/Payment.java
@@ -1,4 +1,49 @@
 package core;
 
+import jdk.nashorn.internal.runtime.regexp.joni.constants.StringType;
+
 public class Payment {
+
+    private String type;
+    private String accountId;
+    private int amount;
+
+
+
+    public Payment(String type , String accountId , int amount){
+
+        this.type = type;
+        this.accountId = accountId;
+        this.amount = amount;
+    }
+
+    public Payment(String type , String accountId , String amount){
+
+        this(type,accountId,Integer.valueOf(amount));
+
+    }
+
+    public boolean isDebtor(){
+
+        return this.type.equals("debtor");
+    }
+
+    public String getType(){
+
+        return  type;
+    }
+
+    public String getAccountId(){
+
+        return getAccountId();
+    }
+
+    public int getAmount(){
+
+        return amount;
+    }
+
+
+
+
 }
